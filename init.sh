ef8e747db6f9175c0fab314a3f981a18e84f7e4d
innns <49517990+innns@users.noreply.github.com>
2023-10-31 17:30:15 +0800

    using gitee instead of github to download ttf

--- init.sh
+++ init.sh
@@ -26,10 +26,16 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 150
 #Download fonts
 mkdir -p ~/Downloads/fonts_MesloLGS
 cd ~/Downloads/fonts_MesloLGS
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+#wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+#wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+#wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+#wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+wget https://gitee.com/keyboardkiller/MesloLGS_NF/raw/master/MesloLGS%20NF%20Regular.ttf
+wget https://gitee.com/keyboardkiller/MesloLGS_NF/raw/master/MesloLGS%20NF%20Bold.ttf
+wget https://gitee.com/keyboardkiller/MesloLGS_NF/raw/master/MesloLGS%20NF%20Italic.ttf
+wget https://gitee.com/keyboardkiller/MesloLGS_NF/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
 sudo chmod 777 *
 cd ..
 #Install fonts
